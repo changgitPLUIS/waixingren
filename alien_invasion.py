a6ee2c55d767af836ce2294ec58d0151f5d021db
changgitPLUIS <2696329466@qq.com>
2022-12-29 12:23:50 +0800

    提交俩个文件，进度12.3.3

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,17 @@
 import sys
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     '''管理游戏资源和行为的类'''
 
     def __init__(self):
         """初始化游戏并创建游戏资源"""
-        self.screen = pygame.display.set_mode((800,600)) #创建一个游戏窗口，宽1200，高800像素
-        pygame.display.set_caption("Alien Invasion") #设置当前窗口标题
-        self.bg_color = (230,230,230)
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
 
     def run_game(self):
         '''开始游戏的主循环'''
@@ -18,7 +21,7 @@ class AlienInvasion:
                     sys.exit()
 
             #每次循环时都重绘屏幕
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             #让最近绘制的屏幕可见
             pygame.display.flip()
